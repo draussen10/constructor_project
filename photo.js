e21c727fac7d186c6497a96ae17b609e83e177ea
draussen10 <a.a.boev@bk.ru>
2022-11-06 00:27:32 +0300

    Добавил cloth-img

---
+++ photo.js
@@ -0,0 +1,9 @@
+document.querySelector('#form-order').addEventListener('submit', e => {
+	e.preventDefault()
+
+	html2canvas(document.querySelector(".imageBox")).then(canvas => {
+		canvas.toBlob(function(blob) {
+			window.saveAs(blob, 'my_image.jpg');
+		});
+	});
+})
